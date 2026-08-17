11630d57e4bedf47e6adb87bc2707e3b6db6ea1b
Kronix <thyagocv8@gmail.com>
2026-08-17 14:38:01 -0300

    Traduz tagall para PT-BR

--- plugins/P-GRUPOS/grupo-tagall.js
+++ plugins/P-GRUPOS/grupo-tagall.js
@@ -1,16 +1,16 @@
 const handler = async (m, { conn, text, participants }) => {
   const mentions = participants.map(p => p.id)
-  const texto = text || '📢 Atención a todos.'
+  const texto = text || '📢 Atenção a todos.'
   const lista = participants.map(p => `▢ @${p.id.split('@')[0]}`).join('\n')
 
-  const txt = `*⌬┤ 📢 ├⌬ TAG ALL.*\n▢ *Mensaje:* ${texto}\n\n${lista}`
+  const txt = `*⌬┤ 📢 ├⌬ MARCAR TODOS.*\n▢ *Mensagem:* ${texto}\n\n${lista}`
 
   await conn.sendMessage(m.chat, { text: txt, mentions }, { quoted: m })
 }
 
-handler.help = ['tagall']
+handler.help = ['marcartodos']
 handler.tags = ['group']
-handler.command = ['tagall', 'mencionartodos', 'invocar', 'todos']
+handler.command = ['tagall', 'mencionartodos', 'invocar', 'todos', 'marcartodos']
 handler.groupOnly = true
 handler.adminOnly = true
 handler.noRegister = true
